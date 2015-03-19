b635c8ba6728235af8f54e1744918928db1c85f6
Smesworld <adodge@uvic.ca>
2015-03-19 14:07:09 -0700

    Added not equ, and calculated equ/adj.

--- generate.py
+++ generate.py
@@ -13,14 +13,13 @@ hands = {} #Dict of hand types, with list of rules for each hand of that type
 """
 Takes a dictionary describing a hand, and the size of the hand.
 Generates a list of card values that are the same.
-
 Eg. Given: {'S3': '2', 'S2': '4', 'S1': '4', 'S5': '4', 'S4': '1', 'C3': '7', 'C2': '9', 'C1': '8', 'hand': '4', 'C5': '9', 'C4': '10'}
 Will return: [(2, '=', 5)] 
 This indicates card 2 is the same as card 5.
-
 """
-def equal( training_dict, hand_size ):
+def equal_card( training_dict, hand_size ):
     same_set = []
+    num_equ = 0
 
     #For each card in the hand
     for card in range(1,hand_size):
@@ -31,20 +30,24 @@ def equal( training_dict, hand_size ):
             #If the current card is the same as the next card, add them to the list
             if training_dict['C'+str(card)] == training_dict['C'+str(next_card)]:
                 same_set += [(card, '=', next_card)]
+                num_equ += 1
+            else:
+            	same_set += [(card, '!', next_card)]
+            	
+    same_set += [('e', num_equ)]
 
     return same_set
 
 """
 Takes a dictionary describing a hand, and the size of the hand.
 Generates a list of card values that are adjacent.
-
 Eg. Given: {'S3': '2', 'S2': '4', 'S1': '4', 'S5': '4', 'S4': '1', 'C3': '7', 'C2': '9', 'C1': '8', 'hand': '4', 'C5': '9', 'C4': '10'}
 Will return: [(1, '+', 2), (1, '+', 3), (1, '+', 5) (2, '+', 4), (4, '+', 5)] 
 This indicates card 1 is adjacent to 2, 1 is adjacent to 3, 1 is adjacent to 5, 2 is adjacent to 4, and 4 is adjacent to 5
-
 """
-def adjacent( training_dict, hand_size ):
+def adjacent_card( training_dict, hand_size ):
     adj_set = []
+    num_adj = 0
 
     #For each card in the hand
     for card in range(1,hand_size):
@@ -56,38 +59,41 @@ def adjacent( training_dict, hand_size ):
             if (training_dict['C'+str(card)] == training_dict['C'+str(next_card)]+1) or (training_dict['C'+str(card)] == training_dict['C'+str(next_card)]-1):
                 #print ('Theres something here...')
                 adj_set += [(card, '+', next_card)]
+                num_adj += 1
+    adj_set += [('a',num_adj)]
 
     return adj_set
 
 """
 Takes a dictionary reader (reading from a csv file).
 Generates rules based on simple assumptions for every hand, and adds them to a dictionary of hand rules.
-
 """
 def generate( training_reader ):
     global hands
 
     for line in training_reader:
         num_cards = 0
+        card = []
 
         #Turn all the values in line into integers
         for key in line:
             line[key] = int(line[key])
+            card += [(key, line[key])]
 
             #Count number of cards in hand
             if fnmatch.fnmatch( key, 'C?'):
                 num_cards += 1
 
-        same = equal( line, num_cards )
-        adj = adjacent( line, num_cards )
+        same = equal_card( line, num_cards )
+        adj = adjacent_card( line, num_cards )
 
-        line['rules'] = same + adj
+        #line['rules'] = same + adj #Not used
 
         #Add rules to respective hand classifications
         if str(line['hand']) not in hands.keys():
-            hands[str(line['hand'])] = [same+adj]
+            hands[str(line['hand'])] = [same+adj+card]
         else:
-            hands[str(line['hand'])].append(same+adj)
+            hands[str(line['hand'])].append(same+adj+card)
 
 #WHEN RUNNING: First arg is training file.
 def main():
@@ -99,7 +105,7 @@ def main():
 
     generate( training_reader )
 
-    print hands
+    print (hands)
 
 if __name__ == "__main__":
     main()
