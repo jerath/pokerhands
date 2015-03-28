f95e331fe6c2c8174cb729a6a41467f1d689323b
Smesworld <adodge@uvic.ca>
2015-03-28 08:35:09 -0700

    Added more detailed comments.
    All functions should be fully commented in roughly the same style.

--- generate.py
+++ generate.py
@@ -11,6 +11,7 @@ So Keys are: S1, S2, S3, S4, S5, C1, C2, C3, C4, C5 and hand (which is our class
 """
 Takes a dictionary describing a hand, and the size of the hand.
 Generates a list of card values that are the same and different.
+Returns this list and the total number of equal relations.
 Eg. Given: {'S3': '2', 'S2': '4', 'S1': '4', 'S5': '4', 'S4': '1', 'C3': '7', 'C2': '9', 'C1': '8', 'hand': '4', 'C5': '9', 'C4': '10'}
 Will return: [(2, '=', 5)] 
 '=' = same value
@@ -25,13 +26,12 @@ def equal_card( training_dict, hand_size ):
     for card in range(1,hand_size):
         #Look at the rest of the cards in the hand
         for next_card in range(card+1,hand_size+1):
-            #print (card, next_card)
 
             #If the current card is the same as the next card, add them to the list
             if training_dict['C'+str(card)] == training_dict['C'+str(next_card)]:
                 same_set += [(card, '=', next_card)]
-                num_equ += 1
-            else:
+                num_equ += 1 #Add to the total number that are equal
+            else: #Else, add that they are different
             	same_set += [(card, '!', next_card)]
             	
     same_set += [('e', num_equ)]
@@ -41,6 +41,7 @@ def equal_card( training_dict, hand_size ):
 """
 Takes a dictionary describing a hand, and the size of the hand.
 Generates a list containing the max, min, and avg card values. Assumes cards can not have a negative value.
+Returns max, min and avg.
 Eg. Given: {'S3': '2', 'S2': '4', 'S1': '4', 'S5': '4', 'S4': '1', 'C3': '7', 'C2': '9', 'C1': '8', 'hand': '4', 'C5': '9', 'C4': '10'}
 Will return: [('m', 10), ('n', 7), ('v', 8.6)] 
 'm' = max
@@ -53,24 +54,27 @@ def maxmin_card( training_dict, hand_size ):
     num_avg = 0
     
     for card in range(1,hand_size+1):
+        #Handles the first round through
         if cur_max == -1:
             cur_max = training_dict['C'+str(card)]
         if cur_min == -1:
             cur_min = training_dict['C'+str(card)]
+        #Sets max and min
         if training_dict['C'+str(card)] > cur_max:
             cur_max = training_dict['C'+str(card)]
         if training_dict['C'+str(card)] < cur_min:
             cur_min = training_dict['C'+str(card)]
 
-        num_avg += training_dict['C'+str(card)]
+        num_avg += training_dict['C'+str(card)] #Total value seen
 
-    num_avg = num_avg / hand_size
+    num_avg = num_avg / hand_size #Calculate avg
 
     return [('n', cur_min),('m', cur_max), ('v', num_avg)]
 
 """
 Takes a dictionary describing a hand, and the size of the hand.
 Generates a list of card suits that are the same and those that are differnent.
+Returns this list and the number of same suit.
 Eg. Given: {'S3': '2', 'S2': '4', 'S1': '4', 'S5': '4', 'S4': '1', 'C3': '7', 'C2': '9', 'C1': '8', 'hand': '4', 'C5': '9', 'C4': '10'}
 Will return: [(2, '=', 5)] 
 '#' = same suit
@@ -85,13 +89,11 @@ def equal_suit( training_dict, hand_size ):
     for card in range(1,hand_size):
         #Look at the rest of the cards in the hand
         for next_card in range(card+1,hand_size+1):
-            #print (card, next_card)
-
-            #If the current card is the same as the next card, add them to the list
+            #If the current suit is the same as the next suit, add them to the list
             if training_dict['S'+str(card)] == training_dict['S'+str(next_card)]:
                 same_suit += [(card, '#', next_card)]
-                num_equ += 1
-            else:
+                num_equ += 1 #Increment same suits.
+            else: #Else not the same suit.
             	same_suit += [(card, '%', next_card)]
             	
     same_suit += [('s', num_equ)]
@@ -114,13 +116,12 @@ def adjacent_card( training_dict, hand_size ):
     for card in range(1,hand_size):
         #Look at the rest of the cards in the hand
         for next_card in range(card+1,hand_size+1):
-            #print (card, next_card)
 
             #If the current card is one greater or one less than the next card, add them to the list
             if (training_dict['C'+str(card)] == (training_dict['C'+str(next_card)]+1)) or (training_dict['C'+str(card)] == (training_dict['C'+str(next_card)]-1)):
-                #print ('Theres something here...')
                 adj_set += [(card, '+', next_card)]
                 num_adj += 1
+            #Handles circular potential for 13 card suits, would need to add detection to calculate number to mod by
             elif((training_dict['C'+str(card)]%12) == (training_dict['C'+str(next_card)]%12)) and (training_dict['C'+str(card)] != training_dict['C'+str(next_card)]):
                 adj_set += [(card, '+', next_card)]
                 num_adj += 1
@@ -132,11 +133,12 @@ def adjacent_card( training_dict, hand_size ):
 """
 Takes a list of dictionaries from training file.
 Generates rules based on simple assumptions for every hand, and adds them to a dictionary of hand rules.
+Returns this dictionary of hand rules.
 """
 def generate( training_reader ):
-
     hands = {}
 
+    #For each hand in the training set
     for line in training_reader:
         num_cards = 0
         card = []
@@ -145,7 +147,7 @@ def generate( training_reader ):
         for key in line:
             line[key] = int(line[key])
             
-            # get rid of the hand key
+            # get rid of the hand key, and create a list of the cards and their values
             if key != 'hand':
                 card += [(key, line[key])]
 
@@ -158,12 +160,10 @@ def generate( training_reader ):
         same = equal_suit( line, num_cards ) #Same and differnt suits
         adj = adjacent_card( line, num_cards ) #Adjacent cards
 
-        #line['rules'] = same + adj #Not used
-
         #Add rules to respective hand classifications
-        if str(line['hand']) not in hands.keys():
+        if str(line['hand']) not in hands.keys(): #First time a class has been seen
             hands[str(line['hand'])] = [equ+maxmin_num+same+adj+card]
-        else:
+        else: #Every other time
             hands[str(line['hand'])].append(equ+maxmin_num+same+adj+card)
     return hands 
 
@@ -173,33 +173,37 @@ For each class, for each hand in that class, compairs rule by rule and eliminate
 Produces a minimum list of rules required to identify a hand classification with 100% coverage.
 """
 def generalize( hands ):
-
     generalized_rules = {}
 
+    #For each classification option for hands
     for key in hands:
-        print ('Hand : ' + key + ' Number: ' + str(len(hands[key])))
-        # rules is the first hand in hands
+        #Prints number of hands of each class used in training
+        #print ('Hand : ' + key + ' Number: ' + str(len(hands[key])))
+
+        #Set the base rules to the rules of the first hand
         rules = hands[key][0]
-        #print ('BASE HAND: ' +  str(hands[key][0]))
+
+        #For each hand of class 'key'
         for hand in hands[key]:
-            #print ('TEST HAND: ' + str(hand))
-            #print ('RULE HAND: ' + str(rules))
-            
+            #Duplicate the list of rules
             new_rules = list(rules)
+            #Go through each rule
             for rule in rules:
-                #print ('Looking at: ' + str(rule) )
+                #If the rule does not exist in the current hand, remove it from the list of rules.
                 if rule not in hand:
                     new_rules.remove(rule)
 
-            rules = list(new_rules)
+            rules = list(new_rules) #Update the list of rules.
 
-        generalized_rules[key] = rules
+        generalized_rules[key] = rules #Add the rules to the generalized set after going through all the hands for that class.
 
     return generalized_rules
 
 """
 Takes a list of hands to be classified, and the rules generated in training.
 Generates rules for each hand to be classified and attempts to classify them using the rules.
+Assumes -1 is not a class.
+Returns a list of dictionaries where each has an 'id' corresponding to the order found in, and a 'class' corresponding to the classification
 
 -1: Could not classify.
 0: Nothing in hand; not a recognized poker hand 
@@ -214,11 +218,9 @@ Generates rules for each hand to be classified and attempts to classify them usi
 9: Royal flush; {Ace, King, Queen, Jack, Ten} + flush
 """
 def classify( test_list, rules ):
-    # test_list is a list of hand dicts
-    # rules is a rules dictionary with key = hand class, value = list of rules
     count = 0
     classified = []
-    # generate a rule for each thing in test list
+    # generate a rule for each hand in test list
     for hand in test_list:
         num_cards = 0
         card = []
@@ -227,74 +229,65 @@ def classify( test_list, rules ):
         #Turn all the values in line into integers
         for key in hand:
             hand[key] = int(hand[key])
-            
-            # get rid of the hand key
+
+            # get rid of the hand key, and create a list of the cards and their values
             if key != 'hand':
-                card += [(key, hand[key])]
+                card += [(key, line[key])]
 
             #Count number of cards in hand
             if fnmatch.fnmatch( key, 'C?'):
                 num_cards += 1
 
-        equ = equal_card( hand, num_cards )
-        maxmin_num = maxmin_card( hand, num_cards )
-        same = equal_suit( hand, num_cards )
-        adj = adjacent_card( hand, num_cards )
+        equ = equal_card( line, num_cards ) #Same and different values
+        maxmin_num = maxmin_card( line, num_cards ) #Max, min and avg
+        same = equal_suit( line, num_cards ) #Same and differnt suits
+        adj = adjacent_card( line, num_cards ) #Adjacent cards
 
-        hand_rules = equ + maxmin_num + same + adj + card
+        hand_rules = equ + maxmin_num + same + adj + card #Compiled list of rules for the hand
 
-        # for the rules for each hand
-        # rule is key
+        classification = '-1' #Indicates unclassified
 
-        classification = '-1'
-
+        #Simmilar to generalize, finds matching set of rules
         for key in rules:
-
             new_rules = []
             for rule in rules[key]:
                 if rule in hand_rules:
                     new_rules += [rule]
 
-            #print 'hand ', hand_rules
-            #print 'new ',new_rules
-            #print 'old ',rules[key]
-
+            #If the final set of rules is the same as the rules for the current class being tested:
             if new_rules == rules[key]:
-                #print 'WE MADE IT'
+                #If this is the first time it matches, set it to this class
                 if classification == '-1':
                     classification = key
                 else:
+                    #If this classification is more specific (longer) than the previous class, set it to the new class
                     if len(rules[key]) > len(rules[classification]):
                         classification = key
-
-            # go through the list of rules for a hand
         
-        classified += [{ 'id':count, 'class':classification}]
-
-            # take the intersect.
-            # if the intersect == the rule that we came up with for the hand class
-            # then this is the rule.
-
+        classified += [{ 'id':count, 'class':classification}] #Add it to the classified list
 
     return classified
-    # classify the hand based on the rules
 
-    # for each rule in rules, take the intersection of the rules of the test hand and the rule.
+"""
+Takes a list of classified dictionaries, and the list of hands used for classification.
+Checks the classification in the dictionary to the known classification from the testing list.
+Tracks the number of correct for each class, and the total number of that class checked.
+(Can print that out.)
+Returns the total number of correct and the total number of incorrect.
 
-    # the one it matches that has the most rules is the hand we choose..
-    # add unclassified component? 
-
-    # evaluate?
-
+c# = Number of correct for # class.
+s# = Number total for # class.
+"""
 def evaluate( classification, test_list):
+    #Initialize totals to 0
     num_right = 0
     num_wrong = 0
-    
     c0 = c1 = c2 = c3 = c4 = c5 = c6 = c7 = c8 = c9 = 0
     s0 = s1 = s2 = s3 = s4 = s5 = s6 = s7 = s8 = s9 = 0
 
+    #For every hand in the test list
     for i in range(0,len(test_list)):
-
+        #Increment the hand class
         if int(test_list[i]['hand']) == 0:
             s0 += 1
         if int(test_list[i]['hand']) == 1:
@@ -316,10 +309,9 @@ def evaluate( classification, test_list):
         if int(test_list[i]['hand']) == 9:
             s9 += 1
 
-
-        #print 'test ', test_list[i]['hand'], ' class ', classification[i]['class']
-
+        #Check the classification vs. the actual class of each hand
         if int(test_list[i]['hand']) == int(classification[i]['class']):
+            #Increment correct if it was correct
             if int(test_list[i]['hand']) == 0:
                 c0 += 1
             if int(test_list[i]['hand']) == 1:
@@ -346,6 +338,8 @@ def evaluate( classification, test_list):
             #print (test_list[i])
             #print ('Expect: ' + str(test_list[i]['hand']) + ' Produced: ' + str(classification[i]['class']))
 
+    """
+    #Used to print the number of correct and total for each classification
     print ('0: Right: ' + str(c0) + ' out of: ' + str(s0))
     print ('1: Right: ' + str(c1) + ' out of: ' + str(s1))
     print ('2: Right: ' + str(c2) + ' out of: ' + str(s2))
@@ -356,6 +350,7 @@ def evaluate( classification, test_list):
     print ('7: Right: ' + str(c7) + ' out of: ' + str(s7))
     print ('8: Right: ' + str(c8) + ' out of: ' + str(s8))
     print ('9: Right: ' + str(c9) + ' out of: ' + str(s9))
+    """
 
     return num_right, num_wrong
 
@@ -365,12 +360,10 @@ Separates out 1/10th of the training data as classified testing data.
 Returns as list of training hands and a list of testing hands.
 """
 def generate_test_training(training_list, mod_num):
-    # use mod to split this list into a list of tests and 
-
     test_list = []
     train_list = []
 
-    # every ith line goes to a test list, the rest to a training set
+    # every mod_numth line goes to a test list, the rest to a training set
     for index in range(len(training_list)):
         if index %10 == mod_num:
             test_list.append(training_list[index])
@@ -378,7 +371,6 @@ def generate_test_training(training_list, mod_num):
             train_list.append(training_list[index])
 
     return test_list, train_list
-    # training dictionaries go to generate and generalize
 
 #WHEN RUNNING: First arg is training file.
 def main():
@@ -387,26 +379,19 @@ def main():
     training_csv = open( training_file )
     training_reader = csv.DictReader(training_csv) #Creates a dict from rows in csvfile, using first row as keys
 
+    #Add all training hands to a list
     training_list = []
     for line in training_reader:
         training_list.append(line)
 
-    # divide training file into 10 parts
+    # divide training list 10 different ways and perform classification on each fold
     for i in range(0,10):
-        test_list, train_list = generate_test_training(training_list, i)
-        
-        hands = generate( train_list )
+        test_list, train_list = generate_test_training(training_list, i) #Separate training and testing
+        hands = generate( train_list ) #Generate rules for each hand
+        rules = generalize( hands ) #Generalize the rules into minimum sets
 
-        # print (hands)
-        rules = generalize( hands )
-
-        # evaluate test data
-        #print (' ++++++++++++++++++++ ', i , ' ++++++++++++++ ')
-        #print (rules) 
-        # compare evaluation to actual hand
-
-        classification = classify(test_list, rules)
-        right, wrong = evaluate(classification, test_list)
+        classification = classify(test_list, rules) #Classify the testing set
+        right, wrong = evaluate(classification, test_list) #Evaluate correctness of classification of testing set
 
         print ('Right = ', right, ' Wrong = ', wrong)
 
